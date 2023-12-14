4c98aff1969f053bb221402743ee25b0e486693e
Christopher Barrera <c.barrera@ensi.lan>
2023-12-14 16:17:59 +0100

    Conception de plusieurs VFX + 1 prefab de flamme pour braséro

---
+++ Assets/Christopher/Proto/Scripts/FireLightSIM.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class FireLightSIM : MonoBehaviour
+{
+    [SerializeField] private Light2D fireLight2D;
+    [SerializeField] private float timerChangingIntensityLight_min;
+    [SerializeField] private float timerChangingIntensityLight_max;
+
+    private float _currentTimerValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentTimerValue = Random.Range(timerChangingIntensityLight_min, timerChangingIntensityLight_max);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_currentTimerValue <= 0)
+        {
+            fireLight2D.volumeIntensity = Random.Range(0.1f, 0.4f);
+            _currentTimerValue = Random.Range(timerChangingIntensityLight_min, timerChangingIntensityLight_max);
+        }
+        else
+        {
+            _currentTimerValue -= Time.deltaTime;
+        }
+    }
+}
